4ee90d40176f629ceb68677f189d2ca0aa640686
guivargas95 <77880134+guivargas95@users.noreply.github.com>
2021-03-27 20:55:52 -0300

    Update Questão5_a_b_c.py

--- Questão5_a_b_c.py
+++ Questão5_a_b_c.py
@@ -1,3 +1,36 @@
+''' Desenvolva um programa contendo uma função que permita ao usuário solicitar o PIB (Arquivo .csv) de um país para um determinado ano. 
+O programa solicita ao usuário o nome do país e o ano desejado.
+Caso o país solicitado ou o ano não sejam válidos, o programa deve informar, na saída, a mensagem: 
+País não disponível.
+ou
+Ano não disponível.
+a depender do tipo de dado não encontrado. 
+Exemplo de saída do programa:
+Informe um país: Brasil
+Informe um ano entre 2013 e 2020: 2020
+PIB Brasil em 2020: US$2.35 trilhões.
+Desenvolva um programa contendo uma função que liste, por país, a estimativa de variação do PIB, em percentual, entre 2013 e 2020.
+Exemplo de saída do programa:
+EUA                  Variação de 34.13% entre 2013 e 2020.
+China                Variação de 70.72% entre 2013 e 2020.
+Japão                Variação de 0.2% entre 2013 e 2020.
+Alemanha             Variação de 9.92% entre 2013 e 2020.
+Reino Unido          Variação de 39.18% entre 2013 e 2020.
+França               Variação de 7.5% entre 2013 e 2020.
+Brasil               Variação de -1.67% entre 2013 e 2020.
+Itália               Variação de 1.88% entre 2013 e 2020.
+Índia                Variação de 94.65% entre 2013 e 2020.
+Rússia               Variação de 0.48% entre 2013 e 2020.
+Canadá               Variação de 11.48% entre 2013 e 2020.
+Coreia do Sul        Variação de 54.62% entre 2013 e 2020.
+Espanha              Variação de 6.47% entre 2013 e 2020.
+México               Variação de 30.95% entre 2013 e 2020.
+Indonésia            Variação de -85.76% entre 2013 e 2020.
+Desenvolva uma função que, para um país, exiba o gráfico da evolução do PIB ao longo dos anos.
+A função deve receber, como entrada, o nome de um país, e exibir o gráfico para todo o período listado na tabela.
+O gráfico deve conter os valores do PIB no eixo das ordenadas (vertical) e os anos no eixo das abscissas (horizontal)
+'''
+
 def solicitar_pib(nome_pais,ano):
     import csv
     resultado = 0
